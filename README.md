4e7d1ef699e5ccf0c22ceaf1af481900e0ac6a26
JaeWoong Hwang <jwngh15@gmail.com>
2018-01-31 10:30:02 +0000

    Edit Code Construction

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
+## **Work out Position Dictionary**
+### Description
+#### - You can easily look at the workout footage with this chatbot service.
 
-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.
-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'
-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, 
-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.
-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
-    ----------------------------------------------------------------- 
+### Used Function
+#### - Kakao Developer tool
+#### - Heroku
 
-
-Welcome to your Rails project on Cloud9 IDE!
-
-To get started, just do the following:
-
-1. Run the project with the "Run Project" button in the menu bar on top of the IDE.
-2. Preview your new app by clicking on the URL that appears in the Run panel below (https://HOSTNAME/).
-
-Happy coding!
-The Cloud9 IDE team
-
-
-## Support & Documentation
-
-Visit http://docs.c9.io for support, or to learn more about using Cloud9 IDE. 
-To watch some training videos, visit http://www.youtube.com/user/c9ide
+### Copyright
+#### - This video is copyrighted by ABS Onlinefit, the YouTube channel.
--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -12,10 +12,8 @@ class KakaoController < ApplicationController
     basic_keyboard = { type: "buttons", buttons: ["가슴", "어깨", "등", "팔", "하체"]}
     user_message = params[:content]
   
-  case user_message
-   when "처음으로"
-     keyboard = basic_keyboard
-   when "가슴"
+  
+   if user_message == "가슴"
      keyboard = {type: "buttons", buttons: ["벤치프레스", "인클라인프레스", "케이블크로스오버", "푸쉬업", "처음으로"]}
       if user_message == "벤치프레스"
         url = "https://youtu.be/KyQiUIBvutI"
@@ -32,7 +30,7 @@ class KakaoController < ApplicationController
       elsif user_message == "처음으로"
       keyboard = basic_keyboard
     end
-   when "어깨"
+   elsif user_message == "어깨"
      keyboard = {type: "buttons", buttons: ["덤벨숄더프레스", "사이드레터럴레이즈", "벤트오버레이즈", "업라이트로우", "처음으로"]}
       if user_message == "덤벨숄더프레스"
         url = "https://youtu.be/KyQiUIBvutI"
@@ -49,7 +47,7 @@ class KakaoController < ApplicationController
       elsif user_message == "처음으로"
       keyboard = basic_keyboard
       end
-   when "등"
+   elsif user_message =="등"
      keyboard = {type: "buttons", buttons: ["풀업", "랫풀다운", "바벨로우", "케이블풀오버", "처음으로"]}
      if user_message == "풀업"
         url = "https://youtu.be/KyQiUIBvutI"
@@ -66,7 +64,7 @@ class KakaoController < ApplicationController
      elsif user_message == "처음으로"
       keyboard = basic_keyboard  
      end
-   when "팔"
+   elsif user_message == "팔"
      keyboard = {type: "buttons", buttons: ["이두", "삼두", "처음으로"]}
      if user_message == "이두"
       keyboard = {type: "buttons", buttons: ["바벨컬", "덤벨컬", "케이블컬", "처음으로"]}
@@ -97,7 +95,7 @@ class KakaoController < ApplicationController
         keyboard = basic_keyboard
       end
     end
-   when "하체"
+   elsif user_message == "하체"
      keyboard = {type: "buttons", buttons: ["스쿼트", "런지", "레그익스텐션", "처음으로"]}
       if user_message == "스쿼트"
         url = "https://youtu.be/KyQiUIBvutI"
@@ -111,9 +109,8 @@ class KakaoController < ApplicationController
       elsif user_message == "처음으로"
         keyboard = basic_keyboard  
       end  
-  end
-    
-    return_message = {
+    end
+    ret urn_message = {
       message: {
           text: user_message,
           message_button: {
@@ -123,8 +120,8 @@ class KakaoController < ApplicationController
           },
       keyboard: home_keyboard
     }
-
-      render json: return_message
+    render json: return_message
   end
 end
+
 
