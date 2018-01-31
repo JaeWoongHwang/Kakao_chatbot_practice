8a141fbd3b379c41cd3b20a215695765305f1885
JaeWoong Hwang <jwngh15@gmail.com>
2018-01-31 09:37:36 +0000

    Connecting Heroku

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -21,13 +21,13 @@ class KakaoController < ApplicationController
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
       when "인클라인프레스"
-        url = "https://youtu.be/KyQiUIBvutI"
+        url = "https://youtu.be/09kkHji5NMs"
         return_url = url
       when "케이블크로스오버"
-        url = "https://youtu.be/KyQiUIBvutI"
+        url = "https://youtu.be/N5PCCuxBWzw"
         return_url = url
       when "푸쉬업"
-      url = "https://youtu.be/KyQiUIBvutI"
+      url = "https://youtu.be/Nsu_xA02ieU"
       return_url = url
    when "어깨"
      keyboard = {type: "buttons", buttons: ["덤벨숄더프레스", "사이드레터럴레이즈", "벤트오버레이즈", "업라이트로우"]}
@@ -45,14 +45,53 @@ class KakaoController < ApplicationController
         return_url = url
    when "등"
      keyboard = {type: "buttons", buttons: ["풀업", "랫풀다운", "바벨로우", "케이블풀오버"]}
+     when "풀업"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+      when "랫풀다운"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+      when "바벨로우"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+      when "케이블풀오버"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
    when "팔"
      keyboard = {type: "buttons", buttons: ["이두", "삼두"]}
      when "이두"
-       keyboard = {type: "buttons", buttons: ["바벨컬", "덤벨컬", "케이블컬"]}
+      keyboard = {type: "buttons", buttons: ["바벨컬", "덤벨컬", "케이블컬"]}
+       when "바벨컬"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+       when "덤벨컬"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+       when "케이블컬"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
      when "삼두"
        keyboard = {type: "buttons", buttons: ["케이블프레스다운", "딥스", "라잉익스텐션"]}
+       when "케이블프레스다운"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+       when "딥스"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+       when "라잉익스텐션"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
    when "하체"
      keyboard = {type: "buttons", buttons: ["스쿼트", "런지", "레그익스텐션"]}
+     when "스쿼트"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+      when "런지"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
+      when "레그익스텐션"
+        url = "https://youtu.be/KyQiUIBvutI"
+        return_url = url
   end
     
     return_message = {
