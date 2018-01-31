f0a95ccc8cdf40812f31a751a3f0b73e71d5486c
JaeWoong Hwang <jwngh15@gmail.com>
2018-01-31 10:06:48 +0000

    Edit Wrong Code

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -16,82 +16,89 @@ class KakaoController < ApplicationController
    when "처음으로"
      keyboard = basic_keyboard
    when "가슴"
-     keyboard = {type: "buttons", buttons: ["벤치프레스", "인클라인프레스", "케이블크로스오버", "푸쉬업"]}
-      when "벤치프레스"
+     keyboard = {type: "buttons", buttons: ["벤치프레스", "인클라인프레스", "케이블크로스오버", "푸쉬업", "처음으로"]}
+      if user_message == "벤치프레스"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "인클라인프레스"
+      elsif user_message == "인클라인프레스"
         url = "https://youtu.be/09kkHji5NMs"
         return_url = url
-      when "케이블크로스오버"
+      elsif user_message == "케이블크로스오버"
         url = "https://youtu.be/N5PCCuxBWzw"
         return_url = url
-      when "푸쉬업"
+      elsif user_message == "푸쉬업"
       url = "https://youtu.be/Nsu_xA02ieU"
       return_url = url
+    end
    when "어깨"
-     keyboard = {type: "buttons", buttons: ["덤벨숄더프레스", "사이드레터럴레이즈", "벤트오버레이즈", "업라이트로우"]}
-      when "덤벨숄더프레스"
+     keyboard = {type: "buttons", buttons: ["덤벨숄더프레스", "사이드레터럴레이즈", "벤트오버레이즈", "업라이트로우", "처음으로"]}
+      if user_message == "덤벨숄더프레스"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "사이드레터럴레이즈"
+      elsif user_message == "사이드레터럴레이즈"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "벤트오버레이즈"
+      elsif user_message == "벤트오버레이즈"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "업라이트로우"
+      elsif user_message == "업라이트로우"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
+      end
    when "등"
-     keyboard = {type: "buttons", buttons: ["풀업", "랫풀다운", "바벨로우", "케이블풀오버"]}
-     when "풀업"
+     keyboard = {type: "buttons", buttons: ["풀업", "랫풀다운", "바벨로우", "케이블풀오버", "처음으로"]}
+     if user_message == "풀업"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "랫풀다운"
+     elsif user_message == "랫풀다운"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "바벨로우"
+     elsif user_message == "바벨로우"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "케이블풀오버"
+     elsif user_message == "케이블풀오버"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
+     end
    when "팔"
-     keyboard = {type: "buttons", buttons: ["이두", "삼두"]}
-     when "이두"
-      keyboard = {type: "buttons", buttons: ["바벨컬", "덤벨컬", "케이블컬"]}
-       when "바벨컬"
+     keyboard = {type: "buttons", buttons: ["이두", "삼두", "처음으로"]}
+     if user_message == "이두"
+      keyboard = {type: "buttons", buttons: ["바벨컬", "덤벨컬", "케이블컬", "처음으로"]}
+       if user_message == "바벨컬"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-       when "덤벨컬"
+       elsif user_message == "덤벨컬"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-       when "케이블컬"
+       elsif user_message == "케이블컬"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-     when "삼두"
-       keyboard = {type: "buttons", buttons: ["케이블프레스다운", "딥스", "라잉익스텐션"]}
-       when "케이블프레스다운"
+      end
+     elsif user_message == "삼두"
+       keyboard = {type: "buttons", buttons: ["케이블프레스다운", "딥스", "라잉익스텐션", "처음으로"]}
+       if user_message == "케이블프레스다운"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-       when "딥스"
+       elsif user_message == "딥스"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-       when "라잉익스텐션"
+       elsif user_message == "라잉익스텐션"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
+      end
+    end
    when "하체"
-     keyboard = {type: "buttons", buttons: ["스쿼트", "런지", "레그익스텐션"]}
-     when "스쿼트"
+     keyboard = {type: "buttons", buttons: ["스쿼트", "런지", "레그익스텐션", "처음으로"]}
+      if user_message == "스쿼트"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "런지"
+      elsif user_message == "런지"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
-      when "레그익스텐션"
+      elsif user_message == "레그익스텐션"
         url = "https://youtu.be/KyQiUIBvutI"
         return_url = url
+      end  
   end
     
     return_message = {
