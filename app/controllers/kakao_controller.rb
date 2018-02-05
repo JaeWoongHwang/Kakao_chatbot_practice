da5d5f01b419e3f7cd5da91f3a0cca37aa74d767
JaeWoong Hwang <jwngh15@gmail.com>
2018-02-05 06:54:38 +0000

    Edit kakao_controller

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -1,7 +1,5 @@
 class KakaoController < ApplicationController
   def keyboard
-   @keyboard = {:type => "buttons", buttons: ["선택 1", "선택 2", "선택 3"]}
-    render json: @keyboard
   end
 
   def message
